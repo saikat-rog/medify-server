d671b2d1f0749cd9dfb7898cdbf6697e4614076d
Saikat Das <saikat.rog@gmail.com>
2025-02-23 19:46:49 +0530

    Update server configuration to use environment variable for port and improve app run settings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from app.config import create_app
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=False)
