b8666e890e8d8a1985b862846f4ba05440938226
Saikat Das <saikat.rog@gmail.com>
2025-02-25 18:33:58 +0530

    Update dotenv loading to allow overrides in scheduler and clean up main application

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def welcome():
     return "bro take meds"
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
