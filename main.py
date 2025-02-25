f49e4470a66bd5f5105645826b07e1642bc1692c
Saikat Das <saikat.rog@gmail.com>
2025-02-25 18:00:13 +0530

    Add welcome route to main application

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 
 app = create_app()
 
+@app.route("/")
+def welcome():
+    return "bro take meds"
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port, debug=False)
