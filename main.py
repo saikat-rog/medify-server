569599694948a2e18fab58f6e724807dbd3bfec5
Saikat Das <saikat.rog@gmail.com>
2025-04-17 00:04:14 +0530

    Uncomment app.run in main.py to enable server execution

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ def welcome():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False)
